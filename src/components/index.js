d92538aec47a0ea3a9f6f8410403f2e65ceee0b5
CodeFromRahul <rahulvish194002@gmail.com>
2024-10-27 18:59:13 +0530

    Added Login page

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import Header from "./Header/Header";
 import Footer from "./footer/Footer";
+import Input from "./Input.jsx";
+import Button from "./Button.jsx" 
 
 import Logoutbtn from "./Header/Logoutbtn.js";
 import Logo from "./Logo";
 
 
-export {Header,Footer,Logoutbtn,Logo}
+export {Header,Input,Button,Footer,Logoutbtn,Logo}
